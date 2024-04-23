7833cc8985139b0c938eeec05b09f9d0f56097ab
Rowan30 <150041546+RowanYoussef@users.noreply.github.com>
2024-04-23 11:55:02 +0200

    implementing alarm-clock

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -92,6 +92,8 @@ struct thread
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
+    /*for alarm clock*/
+    int64_t threadBlockingTime;
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
