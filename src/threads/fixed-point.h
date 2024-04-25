b437c7c5df4725c13f1d73840defd39278907f07
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-04-25 16:25:05 -0700

    Implementing the functions in fixed-point.h

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -7,38 +7,52 @@ struct real {
 };
 
 struct real
-convert_int_to_fixed(int n);
+convert_int_to_fixed(int n) {
+    int val = n * real_f;
+    struct real result = { val };
+    return result;
+}
 
 int
-convert_x_to_int_zero(struct real x);
+convert_x_to_int_zero(struct real x) {
+    return x.value / real_f;
+}
 
 int
-convert_x_to_int_nearest(struct real x);
+convert_x_to_int_nearest(struct real x) {
+    if (x >= 0)
+        return (x.value + real_f / 2) / real_f;
+    else
+        return (x.value - real_f / 2) / real_f;
+}
 
 struct real
-real_add(struct real x, struct real y);
+real_add_real(struct real x, struct real y) {
+    struct real res;
+    res.value = x.value + y.value;
+    return res;
+}
 
 struct real
-real_add_real(struct real x, struct real y);
+real_add_int(struct real x, int y) {
+    struct real res;
+    res.value = x.value + (y * real_f);
+    return res;
+}
 
 struct real
-real_add_int(struct real x, struct real y);
+real_sub_real(struct real x, struct real y) {
+    struct real res;
+    res.value = x.value - y.value;
+    return res;
+}
 
 struct real
-real_sub_real(struct real x, struct real y);
+real_sub_int(struct real x, int y) {
+    struct real res;
+    res.value = x.value - (y * real_f);
+    return res;
+}
 
 struct real
-real_sub_int(struct real x, struct real y);
-
-struct real
-real_mul_real(struct real x, struct real y);
-
-struct real
-real_mul_int(struct real x, struct real y);
-
-struct real
-real_div_real(struct real x, struct real y);
-
-struct real
-real_div_int(struct real x, struct real y);
-
+real_mul_real(struct real x, struct real y) {
